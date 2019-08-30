d419a7d6ce303a1789dd757e93397da02fc49689
Ahsan <ahsan3200@iiu.edu.pk>
2019-08-30 11:11:08 -0700

    add geolocation from geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -15,8 +15,14 @@ socket.on("connect", function() {
   });
 });
 
-socket.on("newEmail", function(email) {
-  console.log("new Email", email);
+socket.on("newLocationMessage", function(message) {
+  var li = jQuery("<li></li>");
+  var a = jQuery('<a target="_blank">My Current Location</a>');
+  li.text(`${message.from}: `);
+
+  a.attr("href", message.url);
+  li.append(a);
+  jQuery("#messages").append(li);
 });
 
 jQuery("#message-form").on("submit", function(e) {
@@ -28,5 +34,25 @@ jQuery("#message-form").on("submit", function(e) {
       text: jQuery("[name=message]").val()
     },
     function() {}
+  );
+});
+
+var locationButton = jQuery("#send-location");
+
+locationButton.on("click", function() {
+  if (!navigator.geolocation) {
+    return alert("GeoLocation  not supported by your browser !");
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    function(position) {
+      socket.emit("createLocationMessage", {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+    },
+    function() {
+      alert("Unable to fetch loction");
+    }
   );
 });
